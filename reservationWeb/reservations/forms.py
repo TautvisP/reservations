d4383b12423536ee9974793cd69e9faff4b77769
TautvisP <tautvydas.pivo@gmail.com>
2025-03-15 21:27:37 +0200

    Added style for client reservation

--- reservationWeb/reservations/forms.py
+++ reservationWeb/reservations/forms.py
@@ -30,7 +30,7 @@ class ClientReservationForm(forms.ModelForm):
     selected_time = forms.ChoiceField(
         choices=[],
         required=True,
-        label='Select Time'
+        label='Pasirinktas laikas'
     )
 
     class Meta:
@@ -39,7 +39,23 @@ class ClientReservationForm(forms.ModelForm):
             'client_name', 'client_last_name', 'address', 'phone_number',
             'trees_count', 'additional_comments', 'trees_under_4m', 'selected_time'
         ]
+        labels = {
+            'client_name': 'Vardas',
+            'client_last_name': 'Pavardė',
+            'address': 'Adresas',
+            'phone_number': 'Telefono numeris',
+            'trees_count': 'Medžių skaičius',
+            'additional_comments': 'Papildomi komentarai',
+            'trees_under_4m': 'Visi medžiai žemesni nei 4m',
+            'selected_time': 'Pasirinktas laikas'
+        }
         widgets = {
+            'client_name': forms.TextInput(attrs={'placeholder': 'Įveskite vardą'}),
+            'client_last_name': forms.TextInput(attrs={'placeholder': 'Įveskite pavardę'}),
+            'address': forms.TextInput(attrs={'placeholder': 'Įveskite adresą'}),
+            'phone_number': forms.TextInput(attrs={'placeholder': 'Įveskite telefono numerį'}),
+            'trees_count': forms.NumberInput(attrs={'placeholder': 'Įveskite medžių skaičių', 'min': '1'}),
+            'additional_comments': forms.Textarea(attrs={'placeholder': 'Įveskite papildomus komentarus', 'rows': 4}),
             'trees_under_4m': forms.CheckboxInput(),
         }
 
@@ -48,3 +64,5 @@ class ClientReservationForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         if reservation:
             self.fields['selected_time'].choices = [(time, time) for time in reservation.available_times]
+            # Add an empty choice as the first option
+            self.fields['selected_time'].choices.insert(0, ('', 'Pasirinkite laiką'))
